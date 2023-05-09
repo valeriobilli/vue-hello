9a597ca6330e6789a20ae3f6a8653eb65e1c5e62
Valerio Billi <valerio.billi18@gmail.com>
2023-05-09 10:00:28 +0200

    add img

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ createApp ({
     data() {
         return {
             message: "Hello Vue Js",
-            className: "title textCenter"
+            className: "title textCenter",
+            source: "https://code.visualstudio.com/assets/docs/nodejs/vuejs/welcome-to-vue.png"
         }
     }
 }).mount('#app')
